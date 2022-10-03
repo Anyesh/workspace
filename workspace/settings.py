cd76632b6f75b9c62778415489ef61117a3ca770
anyesh <anish.shrestha@alayacare.com>
2022-10-03 16:22:20 +1100

    Update env serach

--- workspace/settings.py
+++ workspace/settings.py
@@ -2,9 +2,8 @@ from pathlib import Path
 
 from dotenv import load_dotenv
 
-load_dotenv()
-
 DEBUG = False
 BASEDIR = Path.home() / ".workspace"
 BASEDIR.mkdir(exist_ok=True)
 ENVDIR = BASEDIR / ".env"
+load_dotenv(ENVDIR)
