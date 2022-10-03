7e05f438242b13f908f14c136e19c99e33dfe679
anyesh <anish.shrestha@alayacare.com>
2022-10-03 13:07:33 +1100

    feat(init-ptoject): Add interfaces

---
+++ workspace/interface/repository.py
@@ -0,0 +1,35 @@
+from abc import ABC, abstractmethod
+
+from workspace.entity.branch import Branch
+
+
+class BaseRepository(ABC):
+    @abstractmethod
+    def create(self, entity) -> None:
+        ...
+
+    @abstractmethod
+    def get(self, *args, **kwargs) -> str:
+        ...
+
+    @abstractmethod
+    def list(self) -> list[str]:
+        ...
+
+
+class AbstractGitRepository(BaseRepository):
+    @abstractmethod
+    def stash(self) -> None:
+        ...
+
+    @abstractmethod
+    def pull(self) -> None:
+        ...
+
+    @abstractmethod
+    def stash_pop(self, entity: Branch) -> None:
+        ...
+
+    @abstractmethod
+    def set(self, entity_name: str) -> None:
+        ...
