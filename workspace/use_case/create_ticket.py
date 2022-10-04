63379df622f284d1536e98b75cd92418bfce46c0
anyesh <anish.shrestha@alayacare.com>
2022-10-04 21:26:32 +1100

    feat: Depricate json db

--- workspace/use_case/create_ticket.py
+++ workspace/use_case/create_ticket.py
@@ -4,15 +4,14 @@ from workspace.interface.repository import BaseRepository
 
 
 class CreateTicket:
-    def __init__(self, repository: BaseRepository):
+    def __init__(self, repository: BaseRepository | None):
         self._repository = repository
 
     def execute(self, input_dto: CreateTicketInputDto) -> Ticket:
 
-        ticket = Ticket(
+        # self._repository.create(ticket)
+        return Ticket(
             id=input_dto.id,
             description=input_dto.description,
             type=input_dto.type,
         )
-        self._repository.create(ticket)
-        return ticket
