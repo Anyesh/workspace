a0a8cfe77c78761110f156fc828ebf1e49de1ec3
anyesh <anish.shrestha@alayacare.com>
2022-10-08 23:29:24 +1100

    feat: Optimize with most common data

--- workspace/cli/service.py
+++ workspace/cli/service.py
@@ -1,7 +1,6 @@
 from __future__ import annotations
 
 import inquirer
-from git.exc import GitCommandError
 from rich.console import Console
 
 from ..dto.create_branch import (
@@ -11,15 +10,36 @@ from ..dto.create_branch import (
     CreateBranchInputDto,
 )
 from ..dto.create_ticket import CreateTicketInputDto
+from ..exception.branch import GitBranchChangeException
 from ..repository.git_repository import GitRepository
-from ..repository.json_repository import JSONRepository
+from ..repository.json_repository import JSONRepository, save_answer
 from ..use_case.checkout_branch import CheckoutBranch
 from ..use_case.create_branch import CreateBranch
 from ..use_case.create_ticket import CreateTicket
-from ..utils import get_loggedin_user
+from ..utils import common_choice_helper, get_loggedin_user
 
 console = Console()
 user = get_loggedin_user()
+
+_cache_repository = JSONRepository()
+
+
+#! TODO: Organize this
+@save_answer(repository=JSONRepository())
+def assist_base_branch_selection(app: str, repository: GitRepository) -> dict:
+    questions = [
+        inquirer.List(
+            "base_branch",
+            message=f"Which branch you want to base '{app}' on?",
+            default="develop",
+            choices=common_choice_helper(
+                repository.list(), _cache_repository.get_recent_branches()
+            ),
+            carousel=True,
+        )
+    ]
+
+    return inquirer.prompt(questions=questions, raise_keyboard_interrupt=True)
 
 
 class CLIService:
@@ -35,16 +55,18 @@ class CLIService:
 
             match_branch = repository.find_by_id(answers.ticket_id)
             if match_branch is None:
-                raise GitCommandError("No branch found with that id", "git branch -a")
+                console.log(f"Branch not found for {app}", style="bold red")
+                continue
+                # raise GitBranchChangeException(
+                #     f"No branch found with id {answers.ticket_id}"
+                # )
 
             # Saving any unsaved changes of current branch
             repository.stash()
 
             # Popping any previos saved changes
             repository.stash_pop(match_branch)
-            input_dto = CheckoutBranchInputDto(
-                name=match_branch,
-            )
+            input_dto = CheckoutBranchInputDto(name=match_branch)
             console.log(f"⚙ checking out {match_branch} ..", style="bold blue")
             CheckoutBranch(repository).execute(input_dto=input_dto)
 
@@ -69,7 +91,7 @@ class CLIService:
                     f"Branch already exists with same id {any_match}",
                     style="bold green",
                 )
-                console.log("⚙ saving any unsaved changes", style="bold orange1")
+                console.log("⚙ saving any unsaved changes", style="bold blue")
                 repository.stash()
                 console.print("Checking out to existing branch", style="bold green")
                 checkout_input_dto = CheckoutBranchInputDto(
@@ -78,42 +100,31 @@ class CLIService:
                 CheckoutBranch(repository).execute(input_dto=checkout_input_dto)
                 continue
 
-            # TODO: Remove this coupling
-            questions = [
-                inquirer.List(
-                    "base_branch",
-                    message=f"Which branch you want to base {app} on?",
-                    default="develop",
-                    choices=repository.list(),
-                    carousel=True,
-                )
-            ]
-
-            answer = inquirer.prompt(questions=questions, raise_keyboard_interrupt=True)
-
+            base_branch_answer = assist_base_branch_selection(app, repository)
             input_dto = CreateBranchInputDto(
                 name=ticket.name_for_branch,
-                base_branch=answer["base_branch"],
+                base_branch=base_branch_answer["base_branch"],
             )
             checkout_input_dto = CheckoutBranchInputDto(
-                name=answer["base_branch"],
+                name=base_branch_answer["base_branch"],
             )
             console.log("⚙ saving any unsaved changes", style="bold orange1")
             repository.stash()
             console.log(
-                f"⚙ checking out {answer['base_branch']} ..", style="bold orange1"
+                f"⚙ checking out '{base_branch_answer['base_branch']}' ..",
+                style="bold green",
             )
             CheckoutBranch(repository).execute(input_dto=checkout_input_dto)
-            console.log("⚙ pulling latest changes", style="bold orange1")
+            console.log("⚙ pulling latest changes", style="bold blue")
 
             if repository.get().tracking_branch() is None:
                 console.log(
-                    "⚠ no tracking branch found. Nohing to pull.", style="bold orange1"
+                    "⚠ no tracking branch found. Nothing to pull.", style="bold orange1"
                 )
             else:
                 repository.pull()
             repository.sync_submodules()
             CreateBranch(repository).execute(input_dto=input_dto)
             console.log(
-                f"⚙ checking out {ticket.name_for_branch} ..", style="bold green"
+                f"⚙ checking out {ticket.name_for_branch} ..", style="bold blue"
             )
